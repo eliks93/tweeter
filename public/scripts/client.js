a50ca9c8fd69df60835c99a3666df8c1c568e4d1
Eli Kittross-Schnell <eliks92@gmail.com>
2019-11-06 22:08:16 +0000

    initial commit... I'm so sorry

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,89 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+
+const createdTime = (createdAt) => {
+  const currentTime = new Date(); 
+  const currentSeconds = currentTime / 1000
+  const seconds = createdAt.created_at / 1000
+  const difference = currentSeconds - seconds;
+  const minutes = difference / 60
+  const hours = minutes / 60
+  const days = hours / 24
+  const months = days /30
+  const years = days / 365
+
+  
+  if(difference < 60) {
+    return "less than a minute";
+  } else if (Math.floor(minutes) < 60) {
+    return `${minutes} minutes ago`;
+  } else if (hours < 24) {
+    return  `${Math.floor(hours)} hours ago`;
+  } else if (days < 30) {
+    return `${Math.floor(days)} days ago`
+  } else if (Math.floor(months) < 12) {
+    return `${Math.floor(months)} months ago`
+  } else {
+    return `${Math.floor(years)} years ago`
+  }
+}
+
+
+const createTweetElement = (tweetData) => {
+  const markUp =$(`
+  <article class="tweets">
+    <img src=${tweetData.user.avatars}>
+    <span class="name">${tweetData.user.name}</span>
+    <span class="handle">${tweetData.user.handle}</span>
+  </header>
+  <div>
+    <p>${tweetData.content.text}</p>
+  </div> 
+  <footer>
+      <span class="time">${createdTime(tweetData)}</span>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      <i class="fas fa-flag"></i>
+    </article>
+`);
+return markUp;
+}
+
+const renderTweet = (tweets) => {
+  for (const tweetData of tweets) {
+    const $tweet = createTweetElement(tweetData); 
+    $('.tweet-container').append($tweet)
+  }
+}
+
+$(document).ready(function() {
+  renderTweet(data)
+})
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+  $('textarea').on('keyup', function() {     
+     let newNum = ($(this).val().length)
+     let counter = $(this).parent().children()[2].innerHTML
+
+     
+     counter = 140 - newNum
+     if(counter >= 0) {
+      $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '');
+     } else {
+      $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '#b02828')
+     }
+
+  })
+  $('textarea').on('keydown', function() {     
+    let newNum = ($(this).val().length)
+    let counter = $(this).parent().children()[2].innerHTML
+
+    
+    counter = 140 - newNum
+    if(counter >= 0) {
+     $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '');
+    } else {
+     $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '#b02828')
+    }
+
+ })
+  
+
+});
