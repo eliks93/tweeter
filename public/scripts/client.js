4e4dfe7751251d8de75ee8440b9f938c301f109f
Eli Kittross-Schnell <eliks92@gmail.com>
2019-11-08 00:25:15 +0000

    fixed linting issues, jQuery call still being seen as linting mistake

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,51 +7,51 @@
 
 // function compares current time to time tweet was posted at and returns a string based on that number
 const createdTime = (createdAt) => {
-  const currentTime = Date.now(); 
-  const currentSeconds = currentTime / 1000
-  const seconds = createdAt.created_at / 1000
+  const currentTime = Date.now();
+  const currentSeconds = currentTime / 1000;
+  const seconds = createdAt.created_at / 1000;
   const difference = currentSeconds - seconds;
   let s = 's';
-  if(difference < 60) {
+  if (difference < 60) {
     return "less than a minute ago";
   } else if (difference < 3600) {
-      if(Math.floor(difference / 60) === 1) {
-        s = "";
-      }
+    if (Math.floor(difference / 60) === 1) {
+      s = "";
+    }
     return `${Math.floor(difference / 60)} minute${s} ago`;
   } else if (difference < 86400) {
-      if(Math.floor(difference / 3600) === 1) {
-        s = "";
-      }
+    if (Math.floor(difference / 3600) === 1) {
+      s = "";
+    }
     return  `${Math.floor(difference / 3600)} hour${s} ago`;
   } else if (difference < 2592000) {
-      if(Math.floor(difference / 86400) === 1) {
-        s = "";
-      }
-    return `${Math.floor(difference / 86400)} day${s} ago`
+    if (Math.floor(difference / 86400) === 1) {
+      s = "";
+    }
+    return `${Math.floor(difference / 86400)} day${s} ago`;
   } else if (difference < 31104000) {
-      if(Math.floor(difference / 2592000) === 1) {
-        s = "";
-      }
-    return `${Math.floor(difference / 2592000)} month${s} ago`
+    if (Math.floor(difference / 2592000) === 1) {
+      s = "";
+    }
+    return `${Math.floor(difference / 2592000)} month${s} ago`;
   } else {
-      if(Math.floor(difference / 31104000) === 1) {
-        s = "";
-      }
-    return `${Math.floor(difference / 31104000)} year${s} ago`
+    if (Math.floor(difference / 31104000) === 1) {
+      s = "";
+    }
+    return `${Math.floor(difference / 31104000)} year${s} ago`;
   }
-}
+};
 // validate text inputs into our tweets to ensure they are text. Invoked in the create tweet element function
 const escape =  function(str) {
   let p = document.createElement('p');
   p.appendChild(document.createTextNode(str));
   return p.innerHTML;
-}
+};
 
 // creates a tweet element with all of the applicable markup
 
 const createTweetElement = (tweetData) => {
-  const markUp =$(`
+  const markUp = $(`
   <article class="tweets">
     <img src=${tweetData.user.avatars}>
     <span class="name">${tweetData.user.name}</span>
@@ -67,26 +67,28 @@ const createTweetElement = (tweetData) => {
       <i class="fas fa-flag"></i>
     </article>
 `);
-return markUp;
-}
+  return markUp;
+};
 // renders all tweets contained in the tweet object database and appends them to the end of our tweet container
 const renderTweet = (tweets) => {
   //sort fucntionality, returns newer tweets first!
-  let sorted = tweets.sort(function(a, b)  { return b.created_at - a.created_at})
+  let sorted = tweets.sort(function(a, b)  {
+    return b.created_at - a.created_at;
+  });
   for (const tweetData of tweets) {
-    const $tweet = createTweetElement(tweetData); 
-    $('.tweet-container').append($tweet)
+    const $tweet = createTweetElement(tweetData);
+    $('.tweet-container').append($tweet);
   }
-}
+};
 
 // load tweets on the page, it first empties the container so the page does not get duplicate tweets
 const loadTweets = function() {
   $('.tweet-container').empty();
   $.ajax('http://localhost:8080/tweets', { method: 'GET' })
     .then(function(tweets) {
-      renderTweet(tweets)
-  })
-}
+      renderTweet(tweets);
+    });
+};
 
 
 
@@ -94,78 +96,78 @@ const loadTweets = function() {
 
 $(document).ready(function() {
 
-  $(window).scroll(function (event) {
+  $(window).scroll(function(event) {
     let scroll = $(window).scrollTop();
-    if(scroll > 400) {
-      $('i').addClass('visible')
+    if (scroll > 400) {
+      $('i').addClass('visible');
     }
-    if(scroll < 400) {
-      $('i').removeClass('visible')
+    if (scroll < 400) {
+      $('i').removeClass('visible');
     }
-});
+  });
 
-// scrolls to top of page when button is clicked 
-$('.to-top').click(function () {
-  $(window).scrollTop(0)
-})
+  // scrolls to top of page when button is clicked
+  $('.to-top').click(function() {
+    $(window).scrollTop(0);
+  });
 
-  //auto-expand for text area... it works, however it does keep adding rows for each extra row you make. Not hugely important given text limit, but if somone had small screen and pasted 5000 characters in it would screw with the formatting severely. 
+  //auto-expand for text area... it works, however it does keep adding rows for each extra row you make. Not hugely important given text limit, but if somone had small screen and pasted 5000 characters in it would screw with the formatting severely.
   $(document)
-    .one('focus.autoExpand', 'textarea.autoExpand', function(){
-        let savedValue = this.value;
-        this.value = '';
-        this.baseScrollHeight = this.scrollHeight;
-        this.value = savedValue;
+    .one('focus.autoExpand', 'textarea.autoExpand', function() {
+      let savedValue = this.value;
+      this.value = '';
+      this.baseScrollHeight = this.scrollHeight;
+      this.value = savedValue;
     })
-    .on('input.autoExpand', 'textarea.autoExpand', function(){
-        let minRows = this.getAttribute('data-min-rows')|0, rows;
-        this.rows = minRows;
-        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 12);
-        this.rows = minRows + rows;
+    .on('input.autoExpand', 'textarea.autoExpand', function() {
+      let minRows = this.getAttribute('data-min-rows') | 0, rows;
+      this.rows = minRows;
+      rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 12);
+      this.rows = minRows + rows;
     });
 
 
   // function toggles a class to move the new tweet container up and down when clicked
-  $( "main" ).toggleClass('move')
-  loadTweets()
+  $("main").toggleClass('move');
+  loadTweets();
   $('#show-tweet').click(function() {
-    $( "main" ).toggleClass('move')
-    if($('main.move')) {
-       $('textarea').focus()
+    $("main").toggleClass('move');
+    if ($('main.move')) {
+      $('textarea').focus();
     }
-  })
+  });
   // function will submit the text area when enter is pressed. also prevents enter from making new lines
   $('textarea').on('keydown', function() {
     let keyPress = $(event.which);
-    if(keyPress[0] === 13) {
+    if (keyPress[0] === 13) {
       event.preventDefault();
       $('.submit-tweet').submit();
     
     }
-  })
+  });
   // handles the submission logic of tweets. Is invoked any time the submit button is pressed or triggered by pressing enter
-  $('.submit-tweet').submit(function(event){
+  $('.submit-tweet').submit(function(event) {
     event.preventDefault();
-    let data = $(this).serialize()
+    let data = $(this).serialize();
     // checks to see if 0 characters have been entered or too many charcters have been entered and prevents the submission of the form and reload of tweets.
-    let counter = $('textarea').val()
+    let counter = $('textarea').val();
     
-    if(counter.length > 140) {
-      $('span.error').text('Too Many Characters')
-      return
+    if (counter.length > 140) {
+      $('span.error').text('Too Many Characters');
+      return;
     } else if (counter.length === 0) {
-        $('span.error').text('Tweet submission left empty')
-        return
+      $('span.error').text('Tweet submission left empty');
+      return;
     } else {
       // the actual submission of the tweets. the error code is set to an empty string to get rid of it when there is no error which will be the case if the function runs this far
-    $('span.error').text('')
-    $.ajax({ type: "POST", url: '/tweets', data: data,})
-      .then(function() {
+      $('span.error').text('');
+      $.ajax({ type: "POST", url: '/tweets', data: data,})
+        .then(function() {
         // resets the text area upon successful submission. The text area will not alter the text if an error is triggered.
-        $('textarea').val('')
-        $('.counter').text('140')
-          loadTweets()
-    })
+          $('textarea').val('');
+          $('.counter').text('140');
+          loadTweets();
+        });
     }
-  })
+  });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,34 +1,34 @@
 
 $(document).ready(function() {
-   // this whole area is used to update the counter and change it's color based on if it gets to too many characters/
+  // this whole area is used to update the counter and change it's color based on if it gets to too many characters/
   $('textarea').on('keyup', function() {
-     // this one checks on keyup, which allows it to update every time a key is released     
-     let newNum = ($(this).val().length)
-     let counter = $(this).parent().children()[2].innerHTML
+    // this one checks on keyup, which allows it to update every time a key is released
+    let newNum = ($(this).val().length);
+    let counter = $(this).parent().children()[2].innerHTML;
 
 
-     counter = 140 - newNum
-     if(counter >= 0) {
+    counter = 140 - newNum;
+    if (counter >= 0) {
       $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '');
-     } else {
-      $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '#b02828')
-     }
-
-  })
-  $('textarea').on('keydown', function() {    
-     // this function checks on keydown, so even if you hold the button down it will still update. 
-    let newNum = ($(this).val().length)
-    let counter = $(this).parent().children()[2].innerHTML
-
-    
-    counter = 140 - newNum
-    if(counter >= 0) {
-     $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '');
     } else {
-     $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '#b02828')
+      $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '#b02828');
     }
 
- })
+  });
+  $('textarea').on('keydown', function() {
+    // this function checks on keydown, so even if you hold the button down it will still update.
+    let newNum = ($(this).val().length);
+    let counter = $(this).parent().children()[2].innerHTML;
+
+    
+    counter = 140 - newNum;
+    if (counter >= 0) {
+      $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '');
+    } else {
+      $(this).parent().children()[2].innerHTML = counter, $('.counter').css('color', '#b02828');
+    }
+
+  });
 
 
 });
